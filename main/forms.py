39c0c40574cf006eb59bd24ea6d787e36ad59416
SH4WEN <sarmientosherwin11@gmail.com>
2025-10-08 10:25:26 +0800

    UPDATE CONTACT TO TEMPORARY UNAVAILABLE

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,22 @@
-
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'UNAVAILABLE'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'UNAVAILABLE'})
+    )
+    subject = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'UNAVAILABLE'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'UNAVAILABLE'})
+    )
 
-
-    # def __init__(self, *args, **kwargs):
-    #     super(ContactForm, self).__init__(*args, **kwargs)
-    #     for field in self.fields.values():
-    #         field.widget.attrs['disabled'] = 'disabled'
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['disabled'] = 'disabled'  
